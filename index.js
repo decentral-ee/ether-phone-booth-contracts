a17fde72c3f730883945d8c9b4d7abeb93693037
Miao ZhiCheng <miao@decentral.ee>
2019-04-25 17:58:48 +0300

    export IERC20 contract

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const TruffleContract = require("truffle-contract");
 module.exports = {
     load: (provider) => {
         let contracts = {
-            PaymentProcessor : TruffleContract(require("./build/contracts/PaymentProcessor.json"))
+            PaymentProcessor : TruffleContract(require("./build/contracts/PaymentProcessor.json")),
+            IERC20 : TruffleContract(require("./build/contracts/IERC20.json"))
         };
         Object.values(contracts).forEach(i => i.setProvider(provider));
         return contracts;
