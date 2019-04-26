2ce093dd07f03c8a66d95840ffbc53bf820d61d4
Miao ZhiCheng <miao@decentral.ee>
2019-04-26 16:56:17 +0300

    export uniswap contracts

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const TruffleContract = require("truffle-contract");
 module.exports = {
     load: (provider) => {
         let contracts = {
+            UniswapFactoryInterface : TruffleContract(require("./build/contracts/UniswapFactoryInterface.json")),
+            UniswapExchangeInterface: TruffleContract(require("./build/contracts/UniswapExchangeInterface.json")),
             PaymentProcessor : TruffleContract(require("./build/contracts/PaymentProcessor.json")),
             IERC20 : TruffleContract(require("./build/contracts/IERC20.json"))
         };
